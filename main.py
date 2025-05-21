b7947ac33086f246eed894b8e7c7ea8119b0db8d
Dylan Joy <43614573+DylanJoy@users.noreply.github.com>
2025-05-21 11:48:18 -0400

    Added Enguins database into main.py and made the uv.lock file.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from dylan-lab01!")
+from palmerpenguins import load_penguins
+import pandas as pd
 
+def main():
+    # Load the penguins dataset
+    penguins = load_penguins()
+    
+    # penguins is a pandas DataFrame
+    print("Penguins dataset loaded successfully!")
+    print(penguins.head())  # show first 5 rows
 
 if __name__ == "__main__":
     main()
